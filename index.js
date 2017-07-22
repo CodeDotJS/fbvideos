90bb2a4f40f1f244d90bd19bbd42faa6365a2b68
Rishi Giri <rushforlinux@gmail.com>
2017-07-22 09:30:10 +0530

    clean

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const got = require('got');
+
+const msg = 'Either the video is deleted or it\'s not shared publicly!';
+
+const lowResolution = link => {
+	return got(link).then(res => {
+		const link = res.body.split('sd_src_no_ratelimit:"')[1].split('",hd_src_no_ratelimit:')[0];
+		return {
+			url: link
+		};
+	}).catch(err => {
+		if (err) {
+			err.message = msg;
+		}
+		return err.message;
+	});
+};
+
+const highResolution = link => {
+	return got(link).then(res => {
+		const link = res.body.split('hd_src_no_ratelimit:"')[1].split('",aspect_ratio:')[0];
+		return {
+			url: link
+		};
+	}).catch(err => {
+		if (err) {
+			err.message = msg;
+		}
+		return err.message;
+	});
+};
+
+exports.low = lowResolution;
+exports.high = highResolution;
